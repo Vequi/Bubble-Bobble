a5796ac9f8964e01f6af230a4971442baa22efe7
Vequi <120988212+Vequi@users.noreply.github.com>
2024-04-14 13:41:16 +0200

    Add files via upload

---
+++ Bubble Bobble v0.5/src/TileMap.h
@@ -0,0 +1,107 @@
+#pragma once
+#include <raylib.h>
+#include "Sprite.h"
+#include "Point.h"
+#include "AABB.h"
+#include "Globals.h"
+#include <unordered_map>
+
+enum class Tile {
+
+	// -1: area covered by an entity
+	EMPTY = -1,
+	//  0: air tile
+	AIR = 0,
+
+	// 0 < id < 50: static tiles
+
+	
+	L1BLOCK = 1, L1BLOCK_NUMT, L1BLOCK_NUMB, L1SHADE_RIGHT, 
+	L1SHADE_CORNER, L1SHADE_B, L1SHADE_BD, L1SHADE_CORNERB, 
+	L1SHADE_3D,
+
+	BLOCK_SQUARE1_TL = 25, BLOCK_SQUARE1_TR, BLOCK_SQUARE1_BL, BLOCK_SQUARE1_BR,
+	BLOCK_SQUARE2_TL, BLOCK_SQUARE2_TR, BLOCK_SQUARE2_BL, BLOCK_SQUARE2_BR,
+	BLOCK_VERT2_T, BLOCK_VERT2_B, BLOCK_HORIZ2_L, BLOCK_HORIZ2_R, BLOCK_BLUE,
+	BLOCK_HORIZ3_L, BLOCK_HORIZ3_M, BLOCK_HORIZ3_R,
+	BLOCK_BEAM_L, BLOCK_BEAM_R,
+	
+	LADDER_L = 21, LADDER_R, LADDER_TOP_L, LADDER_TOP_R,
+	LOCK_RED = 45, LOCK_YELLOW, 
+	LASER_L = 43, LASER_R,
+
+	// 50 <= id < 100: special tiles
+	DOOR = 50,
+	KEY_RED = 60, YELLOW_KEY, ITEM_APPLE, ITEM_CHILI,
+	LASER = 70, LASER_FRAME0, LASER_FRAME1, LASER_FRAME2,
+
+	// id >= 100: entities' initial locations
+	PLAYER = 100, PLAYER2,
+
+	//Intervals
+	STATIC_FIRST = L1BLOCK,
+	STATIC_LAST = L1BLOCK_NUMB,
+	SOLID_FIRST = L1BLOCK,
+	SOLID_LAST = L1BLOCK_NUMB,
+	SPECIAL_FIRST = DOOR,
+	SPECIAL_LAST = LASER,
+	ENTITY_FIRST = PLAYER,
+	ENTITY_LAST = PLAYER
+};
+
+class TileMap
+{
+public:
+	TileMap();
+	~TileMap();
+
+	AppStatus Initialise();
+	AppStatus Load(int data[], int w, int h);
+	void Update();
+	void Render();
+	void Release();
+
+	//Test for collisions with walls
+	bool TestCollisionWallLeft(const AABB& box) const;
+	bool TestCollisionWallRight(const AABB& box) const;
+	
+	//Test collision with the ground and update 'py' with the maximum y-position to prevent
+	//penetration of the grounded tile, that is, the pixel y-position above the grounded tile.
+	//Grounded tile = solid tile (blocks) or ladder tops.
+	bool TestCollisionGround(const AABB& box, int *py) const;
+	
+	//Test if there is a ground tile one pixel below the given box
+	bool TestFalling(const AABB& box) const;
+	
+	//Test if box is on ladder and update 'px' with the x-center position of the ladder
+	bool TestOnLadder(const AABB& box, int* px) const;
+	
+	//Test if box is on ladder top and update 'px' with the x-center position of the ladder
+	bool TestOnLadderTop(const AABB& box, int* px) const;
+
+private:
+	void InitTileDictionary();
+
+	Tile GetTileIndex(int x, int y) const;
+	bool IsTileSolid(Tile tile) const;
+	bool IsTileLadderTop(Tile tile) const;
+	bool IsTileLadder(Tile tile) const;
+	bool CollisionX(const Point& p, int distance) const;
+	bool CollisionY(const Point& p, int distance) const;
+	int GetLadderCenterPos(int pixel_x, int pixel_y) const;
+
+	//Tile map
+	Tile *map;
+
+	//Size of the tile map
+	int size, width, height;
+	
+	//Dictionary of tile frames
+	std::unordered_map<int, Rectangle> dict_rect;
+
+	Sprite *laser;
+	
+	//Tile sheet
+	const Texture2D *img_tiles;
+};
+

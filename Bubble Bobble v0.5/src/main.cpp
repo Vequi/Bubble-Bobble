9c8f78ab10938012f9147b6dcca447e72335432c
Vequi <oscarvequil@gmail.com>
2024-06-02 19:29:41 +0200

    fs

--- Bubble Bobble v0.5/src/main.cpp
+++ Bubble Bobble v0.5/src/main.cpp
@@ -13,6 +13,21 @@ int main()
     LOG("Application start");
     game = new Game();
     status = game->Initialise(GAME_SCALE_FACTOR);
+    if (!IsWindowFullscreen())
+    {
+        int monitor = GetCurrentMonitor();
+        SetWindowSize(GetMonitorWidth(monitor), GetMonitorHeight(monitor));
+        ToggleFullscreen();
+        SetWindowPosition(GetMonitorWidth(monitor) / 3, 0);
+    }
+    else
+    {
+        int monitor = GetCurrentMonitor();
+        ToggleFullscreen();
+        SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        SetWindowPosition(GetMonitorWidth(monitor) / 3, 0);
+    }
+
     if (status != AppStatus::OK)
     {
         LOG("Failed to initialise game");
